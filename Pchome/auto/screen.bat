f01f72f07842784e64c8030ac398283cbf323525
YEN <cglogc53112@gmail.com>
2020-05-13 01:20:56 +0800

    structure change

--- Pchome/auto/screen.bat
+++ Pchome/auto/screen.bat
@@ -2,6 +2,7 @@ REM cd /d C:\Users\YEN\Desktop\Crawler\Pchome
 REM call activate dev
 
 cd /d %~dp0
-cd ../..
-call bin\venv\Scripts\activate dev
+cd ../
+REM call bin\venv\Scripts\activate dev
 call python screen.py
+
